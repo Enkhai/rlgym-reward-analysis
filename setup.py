eb9f05abc2b497a0787bd87774be0e5bdac27b7f
Enkhai <aerogtx@gmail.com>
2022-03-31 18:39:22 +0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rlgym-analysis',
+    version="0.1",
+    author="Will Moschopoulos",
+    description="A python API for analyzing RLGym rewards using data visualization and analysis techniques.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Enkhai/rlgym-analysis",
+    project_urls = {
+        "Bug Tracker": "https://github.com/Enkhai/rlgym-analysis/issues",
+    },
+    license="MIT",
+    packages=['rlgym_analysis'],
+    install_requires=[
+        'scipy',
+        'pandas',
+        'seaborn',
+        'rlgym>=1.1.0'
+    ]
+)
